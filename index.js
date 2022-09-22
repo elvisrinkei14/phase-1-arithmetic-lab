64c30acaa8ac8d65ff7856997e22dc0a9b9bebd3
Moringa School <elvis.rinkei@moringaschool.com>
2022-09-22 16:57:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 =2
+const num2 =31
+const multiply =num1*num2
+const random =(num1)
+Math.random(num1);
+const num3 =32
+const num4 =7
+const mod = num3%num4
+let max=20
+Math.max=(1,2,3,4,5,20)
